c0439c2d43645db45bac42530d93dac0cb7a7326
aAmir-90 <152504170+aAmir-90@users.noreply.github.com>
2024-04-22 19:35:47 +0530

    Port update

--- server.js
+++ server.js
@@ -9,14 +9,14 @@ require('dotenv').config();
 const bodyParser = require('body-parser');
 app.use(bodyParser.json())
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 6000;
 
-const userRoutes = require('./routes/userRoutes');
-const candidateRoutes = require('./routes/candidateRoutes');
+const userRoutes = require('./routes/userRoutes.js');
+const candidateRoutes = require('./routes/candidateRoutes.js');
 
 app.use('/user', userRoutes);
 app.use('/candidate', candidateRoutes);
 
 app.listen(PORT, ()=>{
-    console.log("Listening on port: 5000")
+    console.log("Listening on port: 6000")
 });
